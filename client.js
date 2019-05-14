c490049696828335d6278831d935204b17a24e6e
Andrew Wong <andrew.j.wong@outlook.com>
2019-05-14 22:31:49 +1000

    Update example client

--- client.js
+++ client.js
@@ -1,6 +1,15 @@
 const { Client, MessagePrefix } = require("./PreSonusAPI");
 let client = new Client("192.168.0.167", 53000);
 
+client.listen();
 client.on(MessagePrefix.Setting, function(data) {
   console.log(data);
 });
+client.connect();
+
+// setInterval(() => {
+//   for (let key in client.metering) {
+//     console.log(key + " " + client.metering[key]);
+//   }
+//   console.log("\n\n");
+// }, 1000);
