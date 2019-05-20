128dda98c6e5604fd08961e6b964c2ceb815827a
Andrew Wong <andrew.j.wong@outlook.com>
2019-05-20 11:40:26 +1000

    Refactor example meter site

--- meter_site/server.js
+++ meter_site/server.js
@@ -1,54 +1,77 @@
 "use strict";
 
+const CONSOLE_HOST = "10.185.192.6";
+const CONSOLE_PORT = 53000;
+
+const SERVER_HOST = "0.0.0.0";
+const SERVER_PORT = 8080;
+
+let emulateConsole = true;
+
+//
+
 const fastify = require("fastify")();
 const socketio = require("socket.io")(fastify.server).of("/meter");
 const path = require("path");
 
-const { Client } = require("../PreSonusAPI");
+let PreSonusAPI;
+if (emulateConsole) {
+  PreSonusAPI = {
+    Client: class DummyClient {
+      /* Dummy client to emulate events */
+      constructor() {
+        this.i = 0; // Emulate rise and fall of values
+        setInterval(() => {
+          // Emit a metering event every 50ms
+          socketio.emit("meter", this.metering);
+        }, 50);
+      }
+      listen() {
+        /* Stub */
+      }
+      connect() {
+        /* Stub */
+      }
+      on() {
+        /* Stub */
+      }
+      get metering() {
+        let data = [];
+        for (var i = 0; i < 32; i++) {
+          data.push(
+            Math.abs(parseInt(Math.random() * 65535 * Math.sin(this.i / 50)))
+          );
+        }
+        this.i++;
+        return {
+          level: data
+        };
+      }
+    }
+  };
+} else {
+  PreSonusAPI = require("../PreSonusAPI");
+}
 
-// const Client = class DummyClient {
-//   // Dummy client to emulate events
-//   constructor() {
-//     this.i = 0;
-//     setInterval(() => {
-//       socketio.emit("meter", this.metering);
-//     }, 50);
-//   }
+console.info("Starting server...");
+let client = new PreSonusAPI.Client(CONSOLE_HOST, CONSOLE_PORT);
 
-//   listen() {}
-
-//   connect() {}
-
-//   on() {}
-
-//   get metering() {
-//     let data = [];
-//     for (var i = 0; i < 32; i++) {
-//       data.push(
-//         Math.abs(parseInt(Math.random() * 65535 * Math.sin(this.i / 50)))
-//       );
-//     }
-//     this.i++;
-//     return {
-//       level: data
-//     };
-//   }
-// };
-
-let client = new Client("10.185.192.6", 53000);
-
+console.debug(`Listening to UDP messages`);
 client.listen();
-client.connect();
-
 client.on("meter", data => socketio.emit("meter", data));
+
+console.debug(`Connecting to console on ${CONSOLE_HOST}:${CONSOLE_PORT}`);
+client.connect();
 
 fastify.register(require("fastify-static"), {
   root: path.join(__dirname, "public")
 });
-fastify.listen(8080, "0.0.0.0", err => {
+
+fastify.listen(SERVER_PORT, SERVER_HOST, err => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
   }
-  console.info(`Server listening on port ${fastify.server.address().port}`);
+  let address = fastify.server.address();
+  console.info(`Server listening on ${address.address}:${address.port}`);
 });
