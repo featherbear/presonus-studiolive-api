88ab6f50b3e292df3a3fd5974d51c922d517a03d
Andrew Wong <andrew.j.wong@outlook.com>
2020-10-20 13:35:34 +1100

    Integrate discovery into the API

--- test.js
+++ test.js
@@ -5,6 +5,9 @@ let client = new Client('192.168.0.50', 53000)
 client.on(MessageTypes.Setting, function (data) {
   console.log(data)
 })
+
+client.on('discover', console.table)
+client.discoverySubscribe()
 client.connect()
 
 // client.meterSubscribe()
