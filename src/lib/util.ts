3d42f218cf398499e7ba0319152dc60607a11644
Andrew Wong <andrew.j.wong@outlook.com>
2020-11-10 03:33:28 +1100

    Refactor constants, add actions and channels

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -3,3 +3,17 @@ export function shortToLE (i) {
   res.writeUInt16LE(i)
   return res
 }
+
+// Hacky mc hack hack
+declare global {
+  interface Buffer {
+    /** 
+     * Returns a boolean indicating if the object partially matches `buffer`
+     */
+    matches(buffer: Buffer): boolean;
+  }
+}
+  
+Buffer.prototype.matches = function (buffer) {
+  return (buffer.length >= this.length) && this.compare(buffer, 0, this.length) === 0
+}
