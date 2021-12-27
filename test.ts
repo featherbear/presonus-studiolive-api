cf3841de71cd98c23a055795a2f2902c5858aca3
Andrew Wong <andrew.j.wong@outlook.com>
2021-12-27 19:16:45 +1100

    this scale sure don't look right

--- test.ts
+++ test.ts
@@ -1,3 +1,5 @@
+import { readFileSync, writeFileSync } from 'fs'
+import { exit } from 'process'
 import { Client, MESSAGETYPES } from './src/api'
 import ZlibPayload from './src/lib/types/ZlibPayload'
 
@@ -39,9 +41,22 @@ client.on(MESSAGETYPES.Setting, function (PV) {
 client.on(MESSAGETYPES.ZLIB, function (ZB: ZlibPayload) {
     if (ZB.id !== 'Synchronize') return
 
+    function intToLE(i) {
+        const res = Buffer.allocUnsafe(4)
+        res.writeUInt32BE(i)
+        return res
+    }
+    let floor = 0x3a970133
     const { chnum, name, username, color, select, solo, volume, mute, pan } = ZB.children.line.children.ch1.values
-    console.log({ chnum, name, username, color, select, solo, volume, mute, pan });
+    console.log({ chnum, name, username, color, select, solo, volume, volume2: intToLE(volume), volume3:
+        
+        intToLE(volume).readInt32LE() === 0 ? 0 : (intToLE(volume).readInt32LE() - floor) / (0X3f800000 - floor), mute, pan });
 
+    // big endian actually hey
+    // ["00000000", "3d76bf3a", "3e018acb", "3e4b90f6", "3ea062b2", "3ec25031", "3ed4d1bb", "3f031597", "3f250315", "3f2e43da", "3f3a9a36", 0, "3f3f3a9b", "3f4a062c", "3f565c88", "3f62b2e6", "3f73a9a2", "3f7b5f9d", "3f800000"]
+
+    // writeFileSync('file.json', JSON.stringify([...JSON.parse(readFileSync('file.json', 'utf8')), intToLE(volume).toString('hex')]))
+    exit(1)
 })
 
 // client.on('discover', console.table)
