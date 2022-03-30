8dafd041e5877a8c0b225c32c9b0b4b79f08c6b3
Andrew Wong <andrew.j.wong@outlook.com>
2022-03-30 15:09:49 +1100

    Update messageTypes.ts
    https://github.com/featherbear/presonus-studiolive-api/issues/13

--- src/lib/constants/messageTypes.ts
+++ src/lib/constants/messageTypes.ts
@@ -1,31 +1,48 @@
 /* eslint no-unused-vars: "off" */
 
 export enum MESSAGETYPES {
+    // KeepAlive
     KeepAlive = 'KA',
+    
     Hello = 'UM',
+    
+    // Message
     JSON = 'JM',
+    
+    // ParamValue
     Setting = 'PV',
+    
+    // ParamStrList
     DeviceList = 'PL',
+    
+    // FileRequest
     FileResource = 'FR',
+    
+    // FileData
     FileResource2 = 'FD',
 
+    // BZ-Message
     ZLIB = 'ZB',
 
+    // Binary
     Unknown1 = 'BO',
 
     /**
      * Compressed content
      */
     Unknown2 = 'CK',
+    Chunk = 'CK',
 
     /**
      * Mute? mt64?
      */
+    // Meter8
     Unknown3 = 'MB',
 
     /**
      * Linear position of faders
      * Only message sent for main fader changes
      */
+    // Meter16
     FaderPosition = 'MS'
 }
