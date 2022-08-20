954a6e0f28b2ad79716a55d8785270263f82bb67
Andrew Wong <andrew.j.wong@outlook.com>
2022-08-21 03:28:56 +1000

    Temporarily disable int64 codes
    JSON.stringify() can't serialise a `bignum`

--- src/lib/util/zlib/zlibDeserialiser.ts
+++ src/lib/util/zlib/zlibDeserialiser.ts
@@ -136,6 +136,9 @@ export function zlibDeserialiseBuffer(buf: Buffer): ZlibPayload {
       // int64
       case 0x4c /* L */: {
         value = valueData.readBigInt64BE()
+        
+        console.warn(`Forcing ${keyData} buffer ${valueData.slice(0, 8).toString('hex')} to be 0}`)
+        value = 0
         break
       }
 
