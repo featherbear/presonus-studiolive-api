e77fc81e7b7cceecc01268a4b06f6c74df577ddd
Andrew Wong <andrew.j.wong@outlook.com>
2022-11-21 00:07:59 +1100

    Cleanup FD parser

--- src/lib/packetParser/FD.ts
+++ src/lib/packetParser/FD.ts
@@ -1,34 +1,42 @@
 /* eslint no-unused-vars: "off" */
 
-/*
-
-Header - first 14 bytes
-
-XX
-
-XX - Message ID
-
-*/
-
 import { UniqueRandom } from '../util/valueUtil'
 
-type Chunk = {
-  max: number,
+interface ChunkSet {
+  /**
+   * Total size of the chunk set
+   */
+  max: number
+
+  /**
+   * Data
+   */
   data: Buffer
 }
+
+/**
+ * Piece chunks of the same ID together
+ */
 const BufferCollector = new class {
   #ids = UniqueRandom.get(16)
-  #files: { [id: number]: Chunk } = {}
+  #files: { [id: number]: ChunkSet } = {}
 
+  /**
+   * Collect a chunk
+   * @returns Completed chunk if available, else nothing
+   */
   put(data: Buffer) {
-    let ret: { id: number, data: Buffer }
+    let ret: {
+      id: number
+      data: Buffer
+    }
 
     const chunk = parseChunk(data)
 
     // TODO: Invalid chunk check
 
     if (!this.#ids.active.includes(chunk.id)) {
-      console.warn('Packet not expected')
+      logger.warn({ id: chunk.id }, 'Received unexpected FD chunk')
       return
     }
 
@@ -39,41 +47,46 @@ const BufferCollector = new class {
         data: chunk.data
       }
     } else {
-      let curr: Chunk
-      if (!(curr = this.#files[chunk.id])) {
-        curr = this.#files[chunk.id] = {
+      let currChunkSet: ChunkSet
+      if (!(currChunkSet = this.#files[chunk.id])) {
+        // Create new entry for the chunk set
+        currChunkSet = this.#files[chunk.id] = {
           max: chunk.totalSize,
           data: Buffer.allocUnsafe(0)
         }
       }
 
-      if (chunk.payloadSize !== chunk.data.length
-      ) {
-        console.warn('Packet inconsistent')
-        return
-      }
-      if (chunk.totalSize !== curr.max ||
-        chunk.bytesRead !== curr.data.length
-      ) {
-        console.warn('Packet not expected')
+      // Ensure the data buffer matches the chunk size
+      if (chunk.payloadSize !== chunk.data.length) {
+        logger.warn({ id: chunk.id, expected: chunk.payloadSize, size: chunk.data.length }, 'FD chunk was inconsistent in size')
         return
       }
 
-      curr.data = Buffer.concat([curr.data, chunk.data])
+      // Ensure consistent state
+      if (chunk.totalSize !== currChunkSet.max ||
+        chunk.bytesRead !== currChunkSet.data.length
+      ) {
+        logger.warn({ id: chunk.id }, 'FD chunk was not consistent with the chunk set information')
+        return
+      }
 
-      if (curr.max === curr.data.length) {
+      currChunkSet.data = Buffer.concat([currChunkSet.data, chunk.data])
+
+      if (currChunkSet.max === currChunkSet.data.length) {
         ret = {
           id: chunk.id,
-          data: curr.data
+          data: currChunkSet.data
         }
       }
     }
 
-    if (ret) {
-      if (this.#files[ret.id]) { delete this.#files[ret.id] }
-      this.#ids.release(ret.id)
-      return ret
+    if (!ret) return
+
+    if (this.#files[ret.id]) {
+      delete this.#files[ret.id]
     }
+    this.#ids.release(ret.id)
+    return ret
   }
 }()
 
@@ -86,12 +99,14 @@ function parseChunk(data: Buffer) {
   const bytesRead = header.readUInt16LE()
   header = header.slice(2)
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const unknown1 = header.slice(0, 2)
   header = header.slice(2)
 
   const totalSize = header.readUInt16LE()
   header = header.slice(2)
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const unknown2 = header.slice(0, 4)
   header = header.slice(4)
 
@@ -100,11 +115,29 @@ function parseChunk(data: Buffer) {
   data = data.slice(14)
 
   return {
+    /**
+     * Chunk ID
+     */
     id,
+
+    /**
+     * Data buffer
+     */
     data,
 
+    /**
+     * Current position / Number of previous bytes in the chunk set
+     */
     bytesRead,
+
+    /**
+     * Total size of the chunk set
+     */
     totalSize,
+
+    /**
+     * Size of the current chunk
+     **/
     payloadSize
   }
 }
