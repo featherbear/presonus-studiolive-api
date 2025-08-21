c437c4371ac9e472b5b0b0137c81d5a00ef617f4
Andrew Wong <andrew.j.wong@outlook.com>
2025-08-22 00:39:57 +1000

    Export parseChannelString in the API

--- src/api.ts
+++ src/api.ts
@@ -4,3 +4,4 @@ export { default as MeterServer } from './lib/MeterServer'
 
 export * from './lib/constants'
 export * from './lib/types'
+export { parseChannelString } from './lib/util/channelUtil'
