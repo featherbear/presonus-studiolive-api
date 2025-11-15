4ca504bc8215e6d75cfa3a790c23cf4e5714849a
Andrew Wong <andrew.j.wong@outlook.com>
2025-11-15 23:04:09 +1100

    Add int16 UBJSON parser

--- src/lib/util/zlib/ubjson.ts
+++ src/lib/util/zlib/ubjson.ts
@@ -3,9 +3,26 @@
  * Partially implements the UBJSON specification
  * https://ubjson.org
  */
+
+enum CharacterMap {
+	"{" = 0x7b,
+	"}" = 0x7d,
+	"[" = 0x5b,
+	"]" = 0x5d,
+
+	I = 0x49,
+	L = 0x4c,
+	S = 0x53,
+	U = 0x55,
+
+	d = 0x64,
+	i = 0x69,
+	l = 0x6c,
+}
+
 export function deserialiseUBJSON<T>(buf: Buffer): T {
 	let idx = 0;
-	if (buf[idx++] !== 0x7b) return null;
+	if (buf[idx++] !== CharacterMap["{"]) return null;
 
 	const rootTree = {};
 	// eslint-disable-next-line @typescript-eslint/no-empty-object-type
@@ -15,7 +32,7 @@ export function deserialiseUBJSON<T>(buf: Buffer): T {
 		let keyData: Buffer | null;
 		if (Array.isArray(workingSet[0])) {
 			// Close leaf array
-			if (buf[idx] === 0x5d /* ] */) {
+			if (buf[idx] === CharacterMap["]"]) {
 				idx++;
 				workingSet.shift();
 				continue;
@@ -24,12 +41,12 @@ export function deserialiseUBJSON<T>(buf: Buffer): T {
 			const controlCharacter = buf[idx++];
 
 			// Close leaf dictionary
-			if (controlCharacter === 0x7d /* } */) {
+			if (controlCharacter === CharacterMap["}"]) {
 				workingSet.shift();
 				continue;
 			}
 
-			if (controlCharacter !== 0x69 /* i */) {
+			if (controlCharacter !== CharacterMap.i) {
 				throw new Error(`(ZB) Failed to find delimiter 1, found ${controlCharacter} instead at position ${idx}`);
 			}
 
@@ -39,129 +56,109 @@ export function deserialiseUBJSON<T>(buf: Buffer): T {
 		}
 
 		const type = buf[idx++];
-		let length = 0;
-		switch (type) {
-			// New leaf dictionary
-			case 0x7b /* { */: {
-				const leaf = {};
 
-				if (Array.isArray(workingSet[0])) {
-					(workingSet[0] as any[]).push(leaf);
-				} else {
-					workingSet[0][keyData.toString()] = leaf;
-				}
-				workingSet.unshift(leaf);
-				continue;
-			}
+		const SkipSymbol = Symbol();
+		function getNextValue(): typeof SkipSymbol | any {
+			switch (type) {
+				// New leaf dictionary
+				case CharacterMap["{"]: {
+					const leaf = {};
 
-			// New leaf array
-			case 0x5b /* [ */: {
-				const leaf = [];
-
-				if (Array.isArray(workingSet[0])) {
-					(workingSet[0] as any[]).push(leaf);
-				} else {
-					workingSet[0][keyData.toString()] = leaf;
+					if (Array.isArray(workingSet[0])) {
+						(workingSet[0] as any[]).push(leaf);
+					} else {
+						workingSet[0][keyData.toString()] = leaf;
+					}
+					workingSet.unshift(leaf);
+					return SkipSymbol;
 				}
 
-				workingSet.unshift(leaf);
-				continue;
-			}
+				// New leaf array
+				case CharacterMap["["]: {
+					const leaf = [];
 
-			// string
-			case 0x53 /* S */: {
-				if (buf[idx++] !== 0x69) {
-					// UBJSON specifications say to read this value as the length type,
-					// but I've yet to see a non-0x69 (i) value in the received payloads
-					throw new Error("(ZB) Failed to find delimiter 2");
+					if (Array.isArray(workingSet[0])) {
+						(workingSet[0] as any[]).push(leaf);
+					} else {
+						workingSet[0][keyData.toString()] = leaf;
+					}
+
+					workingSet.unshift(leaf);
+					return SkipSymbol;
 				}
 
-				length = buf[idx++];
-				break;
-			}
+				// int16
+				case CharacterMap.I: {
+					const dataLength = 2;
+					const valueData = buf.slice(idx, idx + dataLength);
+					idx += dataLength;
+					return valueData.readInt16BE();
+				}
 
-			// float32
-			case 0x64 /* d */: {
-				length = 4;
-				break;
-			}
+				// int64
+				case CharacterMap.L: {
+					const dataLength = 8;
+					const valueData = buf.slice(idx, idx + dataLength);
+					idx += dataLength;
+					return valueData.readBigInt64BE();
+				}
 
-			// int8
-			case 0x69 /* i */: {
-				length = 1;
-				break;
-			}
+				// string
+				case CharacterMap.S: {
+					if (buf[idx++] !== CharacterMap.i) {
+						// UBJSON specifications say to read this value as the length type,
+						// but I've yet to see a non-0x69 (i) value in the received payloads
+						throw new Error("(ZB) Failed to find delimiter 2");
+					}
 
-			// uint8
-			case 0x55 /* U */: {
-				length = 1;
-				break;
-			}
+					const dataLength = (length = buf[idx++]);
+					const valueData = buf.slice(idx, idx + dataLength);
+					idx += dataLength;
+					return valueData.toString();
+				}
 
-			// int32
-			case 0x6c /* l */: {
-				length = 4;
-				break;
-			}
+				// uint8
+				case CharacterMap.U: {
+					const dataLength = 1;
+					const valueData = buf.slice(idx, idx + dataLength);
+					idx += dataLength;
+					return valueData.readUInt8();
+				}
 
-			// int64
-			case 0x4c /* L */: {
-				length = 8;
-				break;
-			}
+				// float32
+				case CharacterMap.d: {
+					const dataLength = 4;
+					const valueData = buf.slice(idx, idx + dataLength);
+					idx += dataLength;
+					return valueData.readFloatBE();
+				}
 
-			default: {
-				throw new Error(`Unknown type ${type} at position ${idx}`);
+				// int8
+				case CharacterMap.i: {
+					const dataLength = 1;
+					const valueData = buf.slice(idx, idx + dataLength);
+					idx += dataLength;
+					return valueData.readInt8();
+				}
+
+				// int32
+				case CharacterMap.l: {
+					const dataLength = 4;
+					const valueData = buf.slice(idx, idx + dataLength);
+					idx += dataLength;
+					return valueData.readInt32BE();
+				}
+
+				default: {
+					throw new Error(`Unknown type ${type} at position ${idx}`);
+				}
 			}
 		}
 
-		const valueData = buf.slice(idx, idx + length);
-
-		let value: any;
-
-		switch (type) {
-			// string
-			case 0x53 /* S */: {
-				value = valueData.toString();
-				break;
-			}
-
-			// float32
-			case 0x64 /* d */: {
-				value = valueData.readFloatBE();
-				break;
-			}
-
-			// int8
-			case 0x69 /* i */: {
-				value = valueData.readInt8();
-				break;
-			}
-
-			// uint8
-			case 0x55 /* U */: {
-				value = valueData.readUInt8();
-				break;
-			}
-
-			// int32
-			case 0x6c /* l */: {
-				value = valueData.readInt32BE();
-				break;
-			}
-
-			// int64
-			case 0x4c /* L */: {
-				value = valueData.readBigInt64BE();
-				break;
-			}
-
-			default: {
-				value = valueData.toString();
-			}
+		const value = getNextValue();
+		if (value === SkipSymbol) {
+			continue;
 		}
-
-		idx += length;
 
 		if (Array.isArray(workingSet[0])) {
 			(workingSet[0] as any[]).push(value);
